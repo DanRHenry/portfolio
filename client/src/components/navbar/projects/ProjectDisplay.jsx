097006f631fd3a5149b40e2797e2798da3a32825
DanRHenry <114960108+DanRHenry@users.noreply.github.com>
2023-07-26 17:25:42 -0400

    added code to hide the close button, renamed toggleprojectinfo to closeprojectinfo

--- client/src/components/navbar/projects/ProjectDisplay.jsx
+++ client/src/components/navbar/projects/ProjectDisplay.jsx
@@ -1,23 +1,13 @@
-import React, { useEffect, useState } from "react";
 import { Button } from "reactstrap";
 
 export default function ProjectDisplay(props) {
-// console.log("props",props)
   let url = props.url;
 
-
-  if (props.infoState === true) {
-document.getElementById("projectInformation").style.display = "none";
-  } else {
-    // document.getElementById("projectInformation").style.display = "inherit";
-  }
-
-  function toggleProjectInfo() {
-    if (props.projectInfo) {
-      // props.projectInfo.style.zIndex = "-1";
+  function closeProjectInfo() {
+    // if (props.projectInfo) {
       props.projectInfo.style.visibility = "hidden";
-      document.getElementById("btncontainer").style.visibility = "hidden";
-    }
+      document.getElementById("projectCloseButton").style.visibility = "hidden";
+    // }
   }
 
   return (
@@ -26,14 +16,13 @@ document.getElementById("projectInformation").style.display = "none";
         <div id="projectInformation">
           <div id="projectInformationContent">
           {props.projectInformation}
-          {/* {props.projectInformation.current} */}
           <br />
           <br />
           </div>
           </div>
       
         <iframe id="projectsiframe" title="display" src={url}></iframe>
-          <div id="btncontainer"><Button id="projectCloseButton" onClick={toggleProjectInfo}>Close</Button></div>
+          <div id="btncontainer"><Button id="projectCloseButton" onClick={closeProjectInfo}>Close</Button></div>
       </div>
     </div>
   );
