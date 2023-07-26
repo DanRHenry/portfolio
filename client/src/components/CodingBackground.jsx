02630b40b13076a74540afdcb4f50da1a7822fb9
DanRHenry <114960108+DanRHenry@users.noreply.github.com>
2023-07-26 17:18:25 -0400

    deleted coding background file and moved image call to app.jsx

--- client/src/components/CodingBackground.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import backgroundCoding from "../media/pankaj-patel-u2Ru4QBXA5Q-unsplash.jpg"
-export default function CodingBackground() {
-  return (
-    <>
-      <img className='CodingBackgroundImg' src= {backgroundCoding} alt="Background" />
-    </>
-  )
-}
