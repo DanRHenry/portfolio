a50f988b6372836296f30c0024b3ed62087e68e0
DanRHenry <114960108+DanRHenry@users.noreply.github.com>
2023-07-26 17:26:48 -0400

    removed props, removed unused imports

--- client/src/components/navbar/aboutMe/AboutMeDisplay.jsx
+++ client/src/components/navbar/aboutMe/AboutMeDisplay.jsx
@@ -1,20 +1,6 @@
-import React, { useEffect, useState } from "react";
-import {Container, Row, Col} from "reactstrap";
-import { Button } from "reactstrap";
 import Headshot from "../../../media/Headshot.png"
 import AboutMeContentFile from "./AboutMeContentFile";
-export default function AboutMeDisplay(props) {
-// console.log("props",props)
-  let url = props.url;
-
-
-  function toggleProjectInfo() {
-    if (props.projectInfo) {
-      // props.projectInfo.style.zIndex = "-1";
-      props.projectInfo.style.visibility = "hidden";
-      document.getElementById("btncontainer").style.visibility = "hidden";
-    }
-  }
+export default function AboutMeDisplay() {
 
   return (
     <>
