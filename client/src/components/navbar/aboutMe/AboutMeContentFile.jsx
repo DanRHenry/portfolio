887ce312b3834a5ed4aaec48f8e7548debd4bec8
DanRHenry <114960108+DanRHenry@users.noreply.github.com>
2023-08-06 11:50:01 -0400

    Added bullets to the section headers

--- client/src/components/navbar/aboutMe/AboutMeContentFile.jsx
+++ client/src/components/navbar/aboutMe/AboutMeContentFile.jsx
@@ -52,11 +52,12 @@ export default function AboutMeContentFile() {
     <div>
     Thank you for visiting my site. <br />
     <div>
-    <div className='bioHeader' onClick={pastCareerToggle} id='togglePastCareer'>My Past Career</div>
+    <div className='bioHeader' onClick={pastCareerToggle} id='togglePastCareer'><ul><li>My Past Career</li></ul></div>
     <div className='bioContent' id='pastCareerContent'>In my previous career, I taught high school music.</div>
-    <div className='bioHeader' onClick={journeyToggle} id='toggleJourney'>My Coding Journey</div>
-    <div className='bioContent' id='journeyContent'>I began to learn how to code in October, 2022, beginning with HTML, CSS, and JavaScript. In the Spring of 2023, I enrolled in a coding boot camp to learn how to develop a full-stack web app using the MERN stack.</div>
-    <div className='bioHeader' onClick={futurePlansToggle} id='toggleFuturePlans'>My Future Plans</div>
+    <div className='bioHeader' onClick={journeyToggle} id='toggleJourney'><ul><li>My Coding Journey</li></ul></div>
+    <div className='bioContent' id='journeyContent'>I began to learn how to code in October, 2022, beginning with HTML, CSS, and JavaScript. In the Spring of 2023, I enrolled in a coding boot camp to learn how to develop a full-stack web app using the MERN stack.
+    </div>
+    <div className='bioHeader' onClick={futurePlansToggle} id='toggleFuturePlans'><ul><li>My Future Plans</li></ul></div>
     <div className='bioContent' id='futurePlansContent'>I am very much looking forward to continuing to learn and develop my coding skills throughout my new career. I have enjoyed all aspects of learning to code, and can see myself happy in a variety of coding roles.</div>
     </div>
     </div>
