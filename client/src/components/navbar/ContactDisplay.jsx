8778eb6b7e02e8887d4aba13ad999543eb5de0be
DanRHenry <114960108+DanRHenry@users.noreply.github.com>
2023-08-06 11:50:50 -0400

    created contact display component to house the content

---
+++ client/src/components/navbar/ContactDisplay.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ContactContent from "./contact/ContactContent";
+export default function ContactDisplay() {
+  return (
+    <div id="contactDisplay">
+      <ContactContent />
+    </div>
+  );
+}
