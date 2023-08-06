dcc0a8f9fe18f70b50ce8319d59b849a97f5a8da
DanRHenry <114960108+DanRHenry@users.noreply.github.com>
2023-08-06 11:45:57 -0400

    added contact menu, added toggles to other menus

--- client/src/components/navbar/Navigation.jsx
+++ client/src/components/navbar/Navigation.jsx
@@ -5,10 +5,18 @@ export default function Navigation(props) {
   function projectsBtnClick () {
     props.aboutMeMenuToggle(false);
     props.projectsMenuToggle(!props.projectsMenuState);
+    props.contactMenuToggle(false)
   }
 
   function aboutMeBtnClick () {
     props.aboutMeMenuToggle(!props.aboutMeMenuState)
+    props.projectsMenuToggle(false)
+    props.contactMenuToggle(false)
+  }
+
+  function contactBtnClick () {
+    props.contactMenuToggle(!props.contactMenuState)
+    props.aboutMeMenuToggle(false)
     props.projectsMenuToggle(false)
   }
   return (
@@ -22,7 +30,7 @@ export default function Navigation(props) {
         <Col id="aboutmebtn" onClick={aboutMeBtnClick}>
           About Me
         </Col>
-        <Col id="contactbtn" onClick={props.projectsMenuToggle}>
+        <Col id="contactbtn" onClick={contactBtnClick}>
           Contact
         </Col>
       </div>
