f37c79b03fc242a5c9994085ef2072e1ee3d9a24
DanRHenry <114960108+DanRHenry@users.noreply.github.com>
2023-08-06 11:46:57 -0400

    added headshot import and reference, linked to aboutmecontentfile

--- client/src/components/navbar/aboutMe/AboutMeDisplay.jsx
+++ client/src/components/navbar/aboutMe/AboutMeDisplay.jsx
@@ -1,15 +1,15 @@
-import Headshot from "../../../media/Headshot.png"
+import Headshot from "../../../media/Headshot.png";
 import AboutMeContentFile from "./AboutMeContentFile";
 export default function AboutMeDisplay() {
-
   return (
     <>
       <div id="aboutMeDisplay">
-        <div id="aboutMeContent"><AboutMeContentFile/>
+        <div id="aboutMeContent">
+          <AboutMeContentFile />
         </div>
-        
+
         <div id="headshotcontainer">
-        <img id="headshotImg" src={Headshot} alt="Headshot" />
+          <img id="headshotImg" src={Headshot} alt="Headshot" />
         </div>
       </div>
     </>
