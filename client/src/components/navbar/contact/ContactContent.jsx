bc1bd7d2cae1a6dc7de2737e8e6064e05e9973cc
DanRHenry <114960108+DanRHenry@users.noreply.github.com>
2023-08-07 13:44:29 -0400

    imported api key, changed first and last to name and organization, set to and from addresses, removed send alert, added email body with labels and html breaks, added fulladdress to send, set the timeout for returning to create message screen after sending, added name and organization to the return, added some console.logs to remove later

--- client/src/components/navbar/contact/ContactContent.jsx
+++ client/src/components/navbar/contact/ContactContent.jsx
@@ -1,86 +1,155 @@
 import React, { useState } from "react";
 import { Form, FormGroup, Label, Col, Input, Button } from "reactstrap";
 import { useRef } from "react";
-
+import key from "../../../helpers/apikey.js";
 export default function ContactContent() {
+  const name = useRef();
+  const organization = useRef();
   const address = useRef();
   const subject = useRef();
   const message = useRef();
   const [active, setActive] = useState(false);
-  // const [formSubmission, setFormSubmission] = useState();
-  // const submitBtn = document.getElementById("submitBtn");
-  // submitBtn.color = "warning";
+  const toAddress = "danhenrydev@outlook.com";
+  const fromAddress = "danielrhenry@gmail.com";
+  // const toAddress = "danielhenrydev@gmail.com"
 
+  // ).then(
+  //   message => alert(message)
+  // );
+  // }
   const handleSubmit = (e) => {
+    const emailBody =
+      "Name: " +
+      `${name.current.value}` +
+      "<br/>" +
+      "Organization: " +
+      `${organization.current.value}` +
+      "<br/>" +
+      "Email Address:" +
+      "<br/>" +
+      `<a href="mailto:${address.current.value}">${address.current.value}</a>` +
+      "<br/>" +
+      "<br/>" +
+      "Message Body:" +
+      "<br/>" +
+      message.current.value;
+    const fulladdress = `https://api.elasticemail.com/v2/email/send?apikey=${key}&subject=${subject.current.value}&from=${fromAddress}&fromName=&sender=${address.current.value}&senderName=${name.current.value}&msgFrom=&msgFromName=&replyTo=&replyToName=&to=${toAddress}&msgTo=&msgCC=&msgBcc=&lists=&segments=&mergeSourceFilename=&dataSource=&channel=&bodyHtml=${emailBody}&bodyText=&charset=&charsetBodyHtml=&charsetBodyText=&template=&headers_firstname=firstname: myValueHere&postBack=&merge_firstname=John&timeOffSetMinutes=&poolName=My Custom Pool&isTransactional=false&attachments=&trackOpens=true&trackClicks=true&utmSource=source1&utmMedium=medium1&utmCampaign=campaign1&utmContent=content1&bodyAmp=&charsetBodyAmp=`;
+    const sendEmail = async () => {
+      let res = await fetch(fulladdress);
+      let result = await res.json();
+      let data = result;
+      console.log("data", data);
+    };
+
     e.preventDefault();
     setActive(!active);
+    console.log("name:", name.current.value);
+    console.log("organization:", organization.current.value);
     console.log("Address:", address.current.value);
     console.log("Subject:", subject.current.value);
     console.log("Message:", message.current.value);
+    sendEmail();
     address.current.value = "";
     subject.current.value = "";
     message.current.value = "";
     setTimeout(() => {
       setActive(false);
-    }, 3000)
+    }, 3000);
   };
 
   return (
     <div id="contactContent">
-      {active ? <div id="sentMessage">Your message has been sent</div> : 
-      
-      <div>
-      <div id="contactGreeting">Send Me a Message</div>
-      <Form onSubmit={handleSubmit}>
-        {/* Email Input */}
-        <FormGroup className="column" row>
-          <Label for="email" sm={2}>
-            Email Address
-          </Label>
-          <Col sm={10}>
-            <Input
-              id="email"
-              name="email"
-              required
-              placeholder="enter email address"
-              innerRef={address}
-              type="email"
-            />
-          </Col>
-        </FormGroup>
+      {active ? (
+        <div id="sentMessage">Your message has been sent</div>
+      ) : (
+        <div>
+          <div id="contactGreeting">Send Me a Message</div>
+          <Form onSubmit={handleSubmit}>
+            {/* Email Input */}
 
-        <FormGroup className="column" row>
-          <Label for="subject" sm={2}>
-            Subject
-          </Label>
-          <Col sm={10}>
-            <Input
-              id="subject"
-              name="text"
-              required
-              placeholder="enter subject"
-              innerRef={subject}
-              type="textarea"
-            />
-          </Col>
-        </FormGroup>
+            {/* First Name */}
+            <FormGroup className="column" row>
+              <Label for="Name" sm={2}>
+                Name
+              </Label>
+              <Col sm={10}>
+                <Input
+                  id="name"
+                  name="text"
+                  required
+                  placeholder="enter name"
+                  innerRef={name}
+                  type="textarea"
+                />
+              </Col>
+            </FormGroup>
+            {/* Last Name */}
+            <FormGroup className="column" row>
+              <Label for="Organization" sm={2}>
+                Organization
+              </Label>
+              <Col sm={10}>
+                <Input
+                  id="organization"
+                  name="text"
+                  placeholder="enter organization name"
+                  innerRef={organization}
+                  type="textarea"
+                />
+              </Col>
+            </FormGroup>
+            {/* Email Address */}
 
-        <FormGroup className="column" row>
-          <Label for="message" sm={2}>
-            Message
-          </Label>
-          <Col sm={10}>
-            <Input
-              id="message"
-              name="text"
-              required
-              placeholder="message body"
-              innerRef={message}
-              type="textarea"
-            />
-          </Col>
-        </FormGroup>
-{/* 
+            <FormGroup className="column" row>
+              <Label for="email" sm={2}>
+                Email Address
+              </Label>
+              <Col sm={10}>
+                <Input
+                  id="email"
+                  name="email"
+                  required
+                  placeholder="enter email address"
+                  innerRef={address}
+                  type="email"
+                />
+              </Col>
+            </FormGroup>
+
+            {/* Subject */}
+            <FormGroup className="column" row>
+              <Label for="subject" sm={2}>
+                Subject
+              </Label>
+              <Col sm={10}>
+                <Input
+                  id="subject"
+                  name="text"
+                  required
+                  placeholder="enter subject"
+                  innerRef={subject}
+                  type="textarea"
+                />
+              </Col>
+            </FormGroup>
+
+            {/* Message Body */}
+            <FormGroup className="column" row>
+              <Label for="message" sm={2}>
+                Message
+              </Label>
+              <Col sm={10}>
+                <Input
+                  id="message"
+                  name="text"
+                  required
+                  placeholder="message body"
+                  innerRef={message}
+                  type="textarea"
+                />
+              </Col>
+            </FormGroup>
+            {/* 
         <FormGroup check row>
           <Col
             sm={{
@@ -88,14 +157,16 @@ export default function ContactContent() {
               size: 10,
             }}
           > */}
-          <div id="submitBtnCnt">
-            <Button color="success" id="submitBtn">Submit</Button>
+            <div id="submitBtnCnt">
+              <Button color="success" id="submitBtn">
+                Submit
+              </Button>
             </div>
-          {/* </Col>
+            {/* </Col>
         </FormGroup> */}
-      </Form>
-      </div>
-      }
+          </Form>
+        </div>
+      )}
     </div>
   );
 }
