2d88656c0a3e0aa35b2023cec1ca080b5c836a12
DanRHenry <114960108+DanRHenry@users.noreply.github.com>
2023-10-23 00:02:11 -0400

    added closedAboutMe Display and Content classes, passed aboutMeMenuState to AboutMeContentFile, took in props

--- client/src/components/navbar/aboutMe/AboutMeDisplay.jsx
+++ client/src/components/navbar/aboutMe/AboutMeDisplay.jsx
@@ -1,11 +1,13 @@
 import Headshot from "../../../media/Headshot.png";
 import AboutMeContentFile from "./AboutMeContentFile";
-export default function AboutMeDisplay() {
+export default function AboutMeDisplay(props) {
   return (
     <>
-      <div id="aboutMeDisplay">
-        <div id="aboutMeContent">
-          <AboutMeContentFile />
+      <div id="aboutMeDisplay" className="closedAboutMeDisplay">
+        <div id="aboutMeContent" className="closedAboutMeContent">
+          <AboutMeContentFile 
+          aboutMeMenuState={props.aboutMeMenuState}
+          />
         </div>
 
         <div id="headshotcontainer">
