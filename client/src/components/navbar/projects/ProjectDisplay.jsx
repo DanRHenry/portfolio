e177d1c61e8659e3b500db6d8bb14e2438750feb
DanRHenry <114960108+DanRHenry@users.noreply.github.com>
2023-10-23 00:06:19 -0400

    added full screen button and function

--- client/src/components/navbar/projects/ProjectDisplay.jsx
+++ client/src/components/navbar/projects/ProjectDisplay.jsx
@@ -1,15 +1,25 @@
+import { Navigate, useNavigate } from "react-router-dom";
 import { Button } from "reactstrap";
 
 export default function ProjectDisplay(props) {
+  // const navigate = useNavigate();
   let url = props.url;
 
   function closeProjectInfo() {
     // if (props.projectInfo) {
       props.projectInfo.style.visibility = "hidden";
       document.getElementById("projectCloseButton").style.visibility = "hidden";
+      document.getElementById("fullScreenProjectButton").style.visibility = "hidden";
     // }
   }
 
+  function openProjectInFullScreen() {
+    window.location.href=url;
+    console.log(url)
+    // return (
+      // navigate(url)
+      // )
+  }
   return (
     <div id="projectContainer">
       <div id="projectDisplay">
@@ -22,7 +32,10 @@ export default function ProjectDisplay(props) {
           </div>
       
         <iframe id="projectsiframe" title="display" src={url}></iframe>
-          <div id="btncontainer"><Button id="projectCloseButton" onClick={closeProjectInfo}>Close</Button></div>
+          <div id="btncontainer"><Button id="projectCloseButton" onClick={closeProjectInfo}>Close</Button>
+          <div id="fullScreenBtn"><Button id="fullScreenProjectButton" onClick={openProjectInFullScreen}>Full Screen</Button></div>
+          </div>
+
       </div>
     </div>
   );
